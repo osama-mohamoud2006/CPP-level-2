1fa6a73ff6dcb15967d315d7169572511acf5eb8
osama mohamoud <wwwhdosama@gmail.com>
2025-07-24 22:00:19 +0300

    Auto commit: 2025-07-24 22:00:18

---
+++ auto_commit.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+project_dir="E:\projects\c++ course\c++ level 2 course 6"
+cd "$project_dir" || exit
+
+created_file=".created_at"
+
+if [ ! -f "$created_file" ]; then
+    created_date=$(date '+%Y-%m-%d_%H-%M-%S')
+    echo "$created_date" > "$created_file"
+    echo "🆕 Created project date: $created_date"
+else
+    created_date=$(cat "$created_file")
+    echo "📅 Project originally created at: $created_date"
+fi
+
+snapshot_dir="E:/projects/_snapshots_level_two"
+snapshot_path="$snapshot_dir/$created_date"
+
+if [ ! -d "$snapshot_path" ]; then
+    mkdir -p "$snapshot_path"
+    cp -r "$project_dir" "$snapshot_path"
+    echo "📁 Snapshot created at: $snapshot_path"
+else
+    echo "📦 Snapshot already exists at: $snapshot_path"
+fi
+
+# Git commands
+git pull origin master
+
+git add -u
+git add .
+
+commit_msg="Auto commit: $(date '+%Y-%m-%d %H:%M:%S')"
+git commit -m "$commit_msg" --quiet
+
+git push origin master --quiet
