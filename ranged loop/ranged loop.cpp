b04b0fc1740fa5c8e2a6c072be458ac6b63900a7
osama mohamoud <wwwhdosama@gmail.com>
2025-07-26 19:20:14 +0300

    Auto commit: 2025-07-26 19:20:14

--- ranged loop/ranged loop.cpp
+++ ranged loop/ranged loop.cpp
@@ -4,15 +4,19 @@ using namespace AllStuff;
 using namespace std;
 int main()
 {
-	int arr[10];
-	array_input(arr, 10); // array_stuff library 
-	cout << endl;
+	//int arr[10];
+	//array_input(arr, 10); // array_stuff library 
+	//cout << endl;
 	// output
 	// you cann't use ranged loop to fill array elements or fill any container it is using only for loop through the elements of an array or a container
-	for (int &output : arr) {
+	/*for (int &output : arr) {
 		cout << output * 10 << endl;
-	}
-
+	}*/
+	int sum = 0; 
+	string str = "32u112";
+	for (char loopthroughstr : str) 
+		(isdigit(loopthroughstr))? sum += char_to_int(loopthroughstr) : sum+=0 ;
 	
+	cout << "the sum= " << sum << endl;
 
 }
