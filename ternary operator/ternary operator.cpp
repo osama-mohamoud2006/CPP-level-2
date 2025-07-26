706d1ac159f5ca938b539fbb8ae883e559decf8f
osama mohamoud <wwwhdosama@gmail.com>
2025-07-26 19:02:27 +0300

    Auto commit: 2025-07-26 19:02:27

--- ternary operator/ternary operator.cpp
+++ ternary operator/ternary operator.cpp
@@ -9,16 +9,21 @@ int number_input() {
 }
 
 int main() {
+	int number = number_input();
 	// check if + or - 
 	// assignment 1
 	// method 1
-	(number_input() > 0)? cout<< "\npostive\n":cout << "\nnegative\n";
+	(number > 0)? cout<< "\npostive\n":cout << "\nnegative\n";
 	// method 2 
-
+	string result = ((number > 0) ?  "\npostive\n" :  "\nnegative\n");
+	cout << "this result 1 " << result << endl;
 
 	// assignment 2
 	/*write program to check if number is zero, postive, negative(using nested ternary operate)*/
-	int number = number_input();
+	// method 1
 	(number == 0) ? cout << "\nit is zero! " : (number > 0) ? cout << "\nit is postive! " : cout << "\nit is negative number!";
+	// method 2
+	string result2 = (number == 0) ?  "\nit is zero! " : (number > 0) ?  "\nit is postive! " :  "\nit is negative number!";
+	cout << "this result 2 " << result2 << endl;
 	cout << "\n";
 }
