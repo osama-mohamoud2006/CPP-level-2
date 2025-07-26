e46e67ea35c07d77be2d2cb000673231b77585ee
osama mohamoud <wwwhdosama@gmail.com>
2025-07-26 19:13:06 +0300

    Auto commit: 2025-07-26 19:13:05

--- ranged loop/ranged loop.cpp
+++ ranged loop/ranged loop.cpp
@@ -1,20 +1,18 @@
-// ranged loop.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
+#include"E:\projects\my library\AllStuff.h" 
+using namespace AllStuff;
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	int arr[10];
+	array_input(arr, 10); // array_stuff library 
+	cout << endl;
+	// output
+	// you cann't use ranged loop to fill array elements or fill any container it is using only for loop through the elements of an array or a container
+	for (int &output : arr) {
+		cout << output * 10 << endl;
+	}
+
+	
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
