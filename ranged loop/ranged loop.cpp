8364ff0417558dee6b99b73a645c3059a78bcdda
osama mohamoud <wwwhdosama@gmail.com>
2025-07-26 20:02:03 +0300

    Auto commit: 2025-07-26 20:02:03

--- ranged loop/ranged loop.cpp
+++ ranged loop/ranged loop.cpp
@@ -1,34 +1,42 @@
 #include <iostream>
 #include"E:\projects\my library\AllStuff.h" 
+#include <string>
 using namespace AllStuff;
 using namespace std;
 int main()
 {
-	//int arr[10];
-	//array_input(arr, 10); // array_stuff library 
-	//cout << endl;
-	// output
+	/* used my lib
+	 used short hand if 
+	 used ranged loop */
+	int arr[10];
+	array_input(arr, 10); // array_stuff library 
+	cout << endl;
+	 
 	// you cann't use ranged loop to fill array elements or fill any container it is using only for loop through the elements of an array or a container
 	/*for (int &output : arr) {
 		cout << output * 10 << endl;
 	}*/
-	//int sum = 0; 
-	//string str = "32u112";
+	int sum = 0; 
+	string str = "32u112";
 	// walkthrough (loop through ) elements of str
-	//for (char loopthroughstr : str) 
-	//	// trenaray if (short hand if)   if true      char_to_int--> in logic lib converts num which is char here to int  // else       
-	//	(isdigit(loopthroughstr))? sum += char_to_int(loopthroughstr) : sum+=0 ;
-	//
-	//cout << "the sum = " << sum << endl;
+	for (char loopthroughstr : str) 
+		// trenaray if (short hand if)   if true      char_to_int--> in logic lib converts num which is char here to int  // else       
+		(isdigit(loopthroughstr))? sum += char_to_int(loopthroughstr) : sum+=0 ;
+	
+	cout << "the sum = " << sum << endl;
 
 
-	int arr[10];
-	array_input(arr, 10);
+	/*int arr[10];
+	array_input(arr, 10);*/
 	cout << endl;
 	/*array_output(arr, 10, "output the array elements using traditional way: ");*/
 	cout << "\n==============================================================\n";
 	// output array 1 using ranged loop
-	for (int output1 : arr) 
+	int max = arr[0];
+	for (int output1 : arr)
+		// trenaray if (short hand if)
+		(output1 > max) ? max = output1 : max +=0 ;
+	cout << "the max is: " << max << endl;
 		/*cout << "array 1's ouput using ranged loop: " << output1 << endl;*/
 	
 cout << endl;
@@ -38,4 +46,11 @@ cout << endl;
 	for (int output2 : arr2)
 		cout << "array2's ouput using ranged loop: " << output2 << endl;
 	cout << endl;
+	string result = "";
+	string name = "Eman";
+	for (char current_char : name)
+		result += toupper(current_char);
+	cout << "your name is: " << result << endl;
+
+
 }
