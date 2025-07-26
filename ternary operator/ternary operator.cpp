5d627b63f604092f679193b22f111f04ad68b96e
osama mohamoud <wwwhdosama@gmail.com>
2025-07-26 16:50:08 +0300

    Auto commit: 2025-07-26 16:50:07

--- ternary operator/ternary operator.cpp
+++ ternary operator/ternary operator.cpp
@@ -1,20 +1,18 @@
-// ternary operator.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+#include"E:\projects\my library\AllStuff.h" 
+using namespace AllStuff;
+using namespace std;
+int number_input() {
+	int number = 0;
+	number = enter_number("just enter anumber: ");
+	return number;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main() {
+	// check if + or - 
+	// assignment 1
+	//(number_input() > 0)? cout<< "\npostive\n":cout << "\nnegative\n";
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	// assignment 2
+
+}
