411e409f40538c7b18cabb33d6a2fc99d012063b
osama mohamoud <wwwhdosama@gmail.com>
2025-07-28 14:00:15 +0300

    Auto commit: 2025-07-28 14:00:14

--- function declration , function defination/function declration , function defination.cpp
+++ function declration , function defination/function declration , function defination.cpp
@@ -1,20 +1,21 @@
-// function declration , function defination.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+ 
+// old way to use functions
 
-int main()
-{
-    std::cout << "Hello World!\n";
+// defination the function
+int input() {
+	cout << "enter number: ";
+	int number = 0; 
+	cin >> number;
+	if (cin.fail()) {
+		cin.clear();
+		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		cout << "\nplease enter vaild number\n";
+		cin >> number;
+	}
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main() {
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+}
