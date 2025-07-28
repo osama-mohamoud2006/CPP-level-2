9e29004eb476b330702f4dcdf405ba3937738803
osama mohamoud <wwwhdosama@gmail.com>
2025-07-28 16:05:14 +0300

    Auto commit: 2025-07-28 16:05:13

--- function declration , function defination/function declration , function defination.cpp
+++ function declration , function defination/function declration , function defination.cpp
@@ -3,24 +3,24 @@ using namespace std;
  
 // old way to use functions
 
-// declration  of  the functions
+//  declaration  of  the functions
 int input();
-int add(int, int);
-int sub(int, int);
-int divison(int, int);
+int add(int a=0  , int b=0  ); // you should pass the data type of parameter that you will define later
+int sub(int a=0, int b=0);
+int divison(int a=1, int b=1);
 
 
 int main() {
 	// call functions
 	int a = input();
 	int b = input();
-	cout << add(a, b) << endl;;
-	cout<<sub(a, b)<<endl;
-	cout << divison(a, b) << endl;
+	cout << add() << endl;
+	//cout<<sub()<<endl; //
+	//cout << divison() << endl;//
 
 }
 
-// defination of functions
+// definition of functions
 int input() {
 
 	cout << "enter number: ";
@@ -34,7 +34,7 @@ int input() {
 	}
 	return number;
 }
-int add(int a , int b) {
+int add(int a , int b ) {
 		return a + b;
 
 	}
