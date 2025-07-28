0e1a3a91d7c4605ff22fc110d20f26688d05414f
osama mohamoud <wwwhdosama@gmail.com>
2025-07-28 18:57:46 +0300

    Auto commit: 2025-07-28 18:57:45

--- Application about functions/Application about functions.cpp
+++ Application about functions/Application about functions.cpp
@@ -5,13 +5,18 @@ using namespace std;
 using namespace AllStuff;
 
 enum enchoice{students_name=1 , students_id=2 , students_age=3 };
-
 enchoice choice();
 
+void array_data(enchoice type);
+
+
+void final_output(enchoice needed);
 int main() {
-	int choice = 0;
+	
+	final_output(choice());
 }
 
+// application on cin failure 
 enchoice choice() {
 	short c = 0;
 	bool ok = false; 
@@ -21,6 +26,35 @@ enchoice choice() {
 		if (cin.fail()) {
 			cin.clear();
 			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			system("color 4F"); //red
+			cout << "\a";
+		}
+		else if (c < 1 || c>3) {
+			cout << "\out of range! ";
+			system("color 4F"); //red
+			cout << "\a";
+		}
+		else {
+			ok = true; 
 		}
 	} while (ok == false);
+	system("color 0F"); //rest color 
+	return (enchoice)c;
+}
+
+//overloading function 
+void array_data(enchoice type) {
+
+}
+
+
+void final_output(enchoice needed) {
+	switch (needed) {
+	case enchoice::students_name:
+
+	case::enchoice::students_id:
+
+	case::enchoice::students_age:
+
+	}
 }
