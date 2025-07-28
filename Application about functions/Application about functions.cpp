2189efe692cc8f85ae0fd7cbe6c60a7f6bfb7e9b
osama mohamoud <wwwhdosama@gmail.com>
2025-07-28 19:27:17 +0300

    Auto commit: 2025-07-28 19:27:16

--- Application about functions/Application about functions.cpp
+++ Application about functions/Application about functions.cpp
@@ -1,60 +1,13 @@
-// program to view students data according to user's choice
-#include <iostream>
-#include"E:\projects\my library\AllStuff.h"  
-using namespace std;
-using namespace AllStuff;
 
-enum enchoice{students_name=1 , students_id=2 , students_age=3 };
-enchoice choice();
-
-void array_data(enchoice type);
-
-
-void final_output(enchoice needed);
-int main() {
-	
-	final_output(choice());
-}
-
-// application on cin failure 
-enchoice choice() {
-	short c = 0;
-	bool ok = false; 
-	do {
-		cout << "enter what you want to view [1] students name , [2] students id , [3] students age: ";
-		cin >> c;
-		if (cin.fail()) {
-			cin.clear();
-			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-			system("color 4F"); //red
-			cout << "\a";
-		}
-		else if (c < 1 || c>3) {
-			cout << "\out of range! ";
-			system("color 4F"); //red
-			cout << "\a";
-		}
-		else {
-			ok = true; 
-		}
-	} while (ok == false);
-	system("color 0F"); //rest color 
-	return (enchoice)c;
-}
-
-//overloading function 
-void array_data(enchoice type) {
-
-}
-
-
-void final_output(enchoice needed) {
-	switch (needed) {
-	case enchoice::students_name:
-
-	case::enchoice::students_id:
-
-	case::enchoice::students_age:
-
-	}
-}
+//?? You are managing information about students.
+//You are building a small system using C++ that :
+//	-Stores each student's name, age, and grades.
+//	- Uses a structure(struct) to group the student's data together.
+//	- Uses arrays to store the student's three grades.
+//	- Uses function overloading to :
+//---- - Print only the name and age.
+//---- - Print the name and the average grade.
+//---- - Print a list of all student names when you have multiple students.
+//You also need to use standard libraries like :
+//-<iostream> for input / output(cout, cin)
+//- <string> for handling text data(student names).
