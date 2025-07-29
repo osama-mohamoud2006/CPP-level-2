7fddbe25ea5b2f5b3852c86aba110d5af890cb25
osama mohamoud <wwwhdosama@gmail.com>
2025-07-29 21:20:04 +0300

    Auto commit: 2025-07-29 21:20:04

--- Application about functions/Application about functions.cpp
+++ Application about functions/Application about functions.cpp
@@ -16,15 +16,15 @@ struct student_data {
 	float grade3 = 0.0;
 };
 
-struct st_total_data {
+struct st_student_collection {
 	student_data student_data[10];
 	
 };
 string input_name();
 short input_age();
 float input_grade();
-st_total_data fill_data(int lenght=1) {
-	st_total_data data;
+st_student_collection fill_data(int lenght=1) {
+	st_student_collection data;
 
 	for (int i = 0; i < lenght; i++) {
 
@@ -37,12 +37,12 @@ st_total_data fill_data(int lenght=1) {
 	return data;
 
 }
-void final_output(st_total_data , int length=1);
-void final_output(st_total_data, bool , int length=1);
+void final_output(st_student_collection , int length=1);
+void final_output(st_student_collection, bool , int length=1);
 
 void start() {
 	bool pass = false;
-	st_total_data fdata =  fill_data(2);
+	st_student_collection fdata =  fill_data(2);
 	cout << "enter what do you want student name only and age or student name and grade? ";
 	int c = 0;
 	do {
@@ -104,7 +104,7 @@ float input_grade() {
 	}
 	return grade;
 }
-void final_output(st_total_data fdata , int length) {
+void final_output(st_student_collection fdata , int length) {
 
 	for (int i = 0; i < length; i++) {
 		cout << "the name: " << fdata.student_data[i].name << endl;
@@ -112,7 +112,7 @@ void final_output(st_total_data fdata , int length) {
 		cout << '\n';
 	}
 }
-void final_output(st_total_data fdata, bool pass , int length) {
+void final_output(st_student_collection fdata, bool pass , int length) {
 
 	for (int i = 0; i < length; i++) {
 		cout << "student name " << fdata.student_data[i].name << endl;
