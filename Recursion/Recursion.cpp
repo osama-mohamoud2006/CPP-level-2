73071e244f6b797274eb6e20eee12ca99f499574
osama mohamoud <wwwhdosama@gmail.com>
2025-07-29 21:46:48 +0300

    Auto commit: 2025-07-29 21:46:47

--- Recursion/Recursion.cpp
+++ Recursion/Recursion.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 using namespace std;
-void sum(int s =0 , int end=0 ){
-    cout<<s<<endl;
-     if(s==end) return ;
-     else{
-        sum(s+1,end);
-     }
+//void sum(int s =0 , int end=0 ){
+//    cout<<s<<endl;
+//     if(s==end) return ;
+//     else{
+//        sum(s+1,end);
+//     }
+//
+//}
 
-}
 int main(){
-     sum(1,5000);
+    /* sum(1,50000000000000);*/
 }
