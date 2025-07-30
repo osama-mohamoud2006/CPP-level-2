1176810fff36915ddaa62570f370c1fb83dc2de7
osama mohamoud <wwwhdosama@gmail.com>
2025-07-30 20:28:40 +0300

    Auto commit: 2025-07-30 20:28:40

--- Recursion/print N to 1 .cpp
+++ Recursion/print N to 1 .cpp
@@ -1,1 +1,14 @@
-#include <
+#include <iostream>
+using namespace std;
+
+void to_n(int n=1 ){
+    if(n!=0){
+        cout<<n<<' ';
+      to_n(n-1);
+    }
+}
+int main(){
+    int n ;
+    cin>>n;
+    to_n(n);
+}
