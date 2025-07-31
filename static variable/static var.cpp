a80bda4318a8122d58e8c9854e811c457f3767d6
osama mohamoud <wwwhdosama@gmail.com>
2025-07-31 15:28:44 +0300

    Auto commit: 2025-07-31 15:28:43

--- static variable/static var.cpp
+++ static variable/static var.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 using namespace std;
  
-int sum(){
-     int n = 0; 
-     // the value will be 1 as the the var life time ends when funtion call end , so it will be intialied 
+int sum(){  
+   //  int n = 0; 
+     // the value will be 1 as the the var life time ends when funtion call end , so it will be intialized 
      // every time i call the function 
-     n++;
+
+    // static int n =1;
+     // the var wonot intilize again as it is "static variable" the value will be the same on the entire
+     // program life time 
+      const int n =1;
+     // it is const wonot change anyways 
+    // n++;
     return n;
 }
 
