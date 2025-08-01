fbeeceed8967d3722db1c9d5c2cffeb6f769daaa
osama mohamoud <wwwhdosama@gmail.com>
2025-08-01 14:41:10 +0300

    Auto commit: 2025-08-01 14:41:10

--- Integer Format (printf) .cpp/printf for int .cpp
+++ Integer Format (printf) .cpp/printf for int .cpp
@@ -1,7 +1,18 @@
-#include <stdio.h>>
+#include <stdio.h>
+#include <cstdio>
 using namespace std;
 
 int main(){
-    /// print int
-    printf("this is int number: %d\n",42);
+    /// print string with int variables
+    int x = 56;
+     int y= 90; 
+     
+    printf("this is int number: %d\n",y); // printing single number
+    printf("the number %d is smaller than %d\n",x,y); // printing 2 numbers
+     
+
+    int s = 1;
+// width specification 
+     printf("the number is: %0*d\n",2,s);
+
 }
