41946d4fc8f76cef7533617badad08a201def74e
osama mohamoud <wwwhdosama@gmail.com>
2025-08-02 19:50:04 +0300

    Auto commit: 2025-08-02 19:50:04

---
+++ 2D array/H.W(muliplication table) .cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <iomanip>
+#include <stdio.h>
+using namespace std;
+
+void header(){
+
+    cout<<"\t"<<"Multplication Table"<<"\t"<<endl;
+}
+
+void ouput(int a[10][10]){
+    cout<<endl;
+    for(int r=0; r<10; r++){ // for rows
+        cout<<"\t"<<r+1<<setw(5)<<"--> ";
+       
+        for(int c=0; c<10; c++) // for columns 
+        {
+            int res = a[r][c];
+            printf("%0*d", 2,res);
+         
+           cout<<" ";
+        }
+                cout<<endl;
+    }
+}
+
+int main(){
+// int multi [10][10];
+ header();
+int res[10][10];
+  for(int r=0; r<10; r++){
+     
+    for(int c= 0; c<10; c++)
+    {
+       res[r][c]= (r+1)*(c+1);
+    }
+ }
+
+  ouput(res);
+
+}
