670fbb9abafda137c40f170d51556405ec22e278
osama mohamoud <wwwhdosama@gmail.com>
2025-08-02 19:58:07 +0300

    Auto commit: 2025-08-02 19:58:06

--- 2D array/H.W(muliplication table) .cpp
+++ 2D array/H.W(muliplication table) .cpp
@@ -11,15 +11,17 @@ void header(){
 void ouput(int a[10][10]){
     cout<<endl;
     for(int r=0; r<10; r++){ // for rows
-        cout<<"\t"<<r+1<<setw(5)<<"--> ";
+        cout<<"\t"<<r+1<<setw(5)<<"-->"<<setw(2);
        
         for(int c=0; c<10; c++) // for columns 
         {
             int res = a[r][c];
+            cout<<(r+1)<<"*"<<(c+1)<<":"<<setw(5);
             printf("%0*d", 2,res);
          
-           cout<<" ";
+           
         }
+        
                 cout<<endl;
     }
 }
