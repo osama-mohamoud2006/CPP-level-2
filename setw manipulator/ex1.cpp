abba99ab0a349bf46b7fa07f046882cd9858bd29
osama mohamoud <wwwhdosama@gmail.com>
2025-08-02 16:56:36 +0300

    Auto commit: 2025-08-02 16:56:36

---
+++ setw manipulator/ex1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+/*
+Num   Square   Cube
+  1        1      1
+  2        4      8
+  3        9     27
+  4       16     64
+  5       25    125
+*/
+
+int main(){
+
+    cout<<"\n"<<setw(5)<<"Num"<<setw(9)<<"Square"<<setw(8)<<"cube"<<endl;
+    
+
+
+
+}
