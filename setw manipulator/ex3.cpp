b88852bf89d0e1a1c7efbc6f76111e46a28c0701
osama mohamoud <wwwhdosama@gmail.com>
2025-08-02 17:09:39 +0300

    Auto commit: 2025-08-02 17:09:39

---
+++ setw manipulator/ex3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+/*
+Name        Score
+Osama         95
+Ali           88
+Mona         100
+
+*/
+
+int main(){
+    string names[]= {"osama","Aya","Mona","Eman","Asia","Menna"};
+    string grades[]={"95","100","90","80","76","90"};
+    
+                cout<<"Name"<<setw(8)<<"Score"<<endl;
+
+
+
+}
