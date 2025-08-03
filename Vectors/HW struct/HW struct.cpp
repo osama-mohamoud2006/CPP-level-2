df5289e10c3579130834e535d6d5bd5952bad60b
osama mohamoud <wwwhdosama@gmail.com>
2025-08-03 19:38:48 +0300

    Auto commit: 2025-08-03 19:38:47

--- Vectors/HW struct/HW struct.cpp
+++ Vectors/HW struct/HW struct.cpp
@@ -3,8 +3,14 @@
 #include <limits>
 using namespace std;
 
+struct stdata{
+    string first_name = "";
+    string last_name = "";
+    short age = 0 ;
+    short salary = 0 ;
+};
 
-int enter_numbre(){
+int enter_number(){
 	short number=0 ;
 	do {
 		cout << "enter number: ";
@@ -20,8 +26,36 @@ int enter_numbre(){
 	return number;
 }
 
+stdata fill_struct_data(){
+	stdata data_input;
+     cout<<"\nenter first name: ";
+       cin>>data_input.first_name;
+
+      cout<<"\nenter last name: ";
+	  cin>>data_input.last_name;
+	  
+	  cout<<"enter age: ";
+	  cin>>data_input.age;
+
+
+
+}
+
+void fill_vector(vector<stdata> &data){
+	int number=0 ;
+	do{
+		cout<<"How many employes: ";
+		number=enter_number();
+
+		for(int i=0; i<number; i++){
+			
+		}
+
+	}
+}
+
 
 
 int main() {
-	cout<< enter_numbre()<<endl;
+	
 }
