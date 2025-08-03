63d885482b807f5504e26fcb1ae10c78d1c955f4
osama mohamoud <wwwhdosama@gmail.com>
2025-08-03 18:14:19 +0300

    Auto commit: 2025-08-03 18:14:18

--- Vectors/H.W_Another ver.cpp
+++ Vectors/H.W_Another ver.cpp
@@ -1,64 +1,39 @@
 #include <iostream>
-#include <vector>
 #include <limits>
+#include <vector>
 #include <iomanip>
+#include <stdio.h>
 using namespace std;
-
-int input_number() {
-    printf("enter number: ");
-    int n = 0;
-
-    cin >> n;
-    while (cin.fail()) {
-        cin.clear();
-        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        system("color 4f");
-        cout << "\a";
-
-        printf("\n enter again: ");
-        cin >> n;
-    }
-    system("color 0f");
-    return n;
-}
-// vector <int> vec
-
-void print(vector<int>&add) {
-    printf("\n\nthe vector's output: ");
-    for (int o : add) {
-        cout << o << " ";
-    }
-    cout << endl;
-}
-
-void start(); 
-
-
-int main() {
-    start();
-}
-
-void start() {
-    vector<int>add;
-    int n = 0;
+ 
+void read_number(vector <int> & add ){
+ 
     char c = 'y';
-    do {
-        cout << setw(5) << "enter the number of elements you want to add: ";
-        cin >> n;
-        for (int i = 0; i < n; i++) {
-            add.push_back(input_number());
-        }
+    
+    while(c=='y'||'Y'){
+        int n =0 ;
+        cout<<"enter number to add to vector: ";
+        cin>>n;
+        add.push_back(n);
 
+        cout<<"\n you want to add another? ";
+        cin>>c;
+        if(c!='y'||c!='Y') break;
+    }
+    
 
-        cout << endl << endl;
-        cout << "do you want to add another ?" << setw(3) << " \n \"Y\" for yes \"N\" to exit \n";
-        printf("\n your choice: ");
-        cin >> c;
-        if (c == 'y' || c == 'Y') {
-            system("cls");
-        }
+}
+ // use & is recommended here 
+void print(vector <int> & add){
+    cout<<"\nvector elements: ";
+    for(int &i : add){
+       printf("%0*d",2,i);
+       cout<<" ";
+    }
+    cout<<endl;
+}
+int main(){
+    vector <int>  add ;
+   read_number( add );
+   print(add);
 
-    } while (c == 'y' || c == 'Y');
-
-    print(add);
 }
