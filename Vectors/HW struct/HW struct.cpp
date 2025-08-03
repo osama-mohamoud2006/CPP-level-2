830a144b64ca02ee3352f8c0f5dfa8a3a3125593
osama mohamoud <wwwhdosama@gmail.com>
2025-08-03 19:49:10 +0300

    Auto commit: 2025-08-03 19:49:09

--- Vectors/HW struct/HW struct.cpp
+++ Vectors/HW struct/HW struct.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <limits>
+#include <iomanip>
 using namespace std;
 
 struct stdata{
@@ -13,7 +14,7 @@ struct stdata{
 int enter_number(){
 	short number=0 ;
 	do {
-		cout << "enter number: ";
+		
 		cin >> number;
 		if (cin.fail() || number < 0) {
 			cin.clear();
@@ -34,28 +35,53 @@ stdata fill_struct_data(){
       cout<<"\nenter last name: ";
 	  cin>>data_input.last_name;
 	  
-	  cout<<"enter age: ";
+	  cout<<"\nenter age: ";
 	  cin>>data_input.age;
 
+	  cout<<"\nenter salary: ";
+	  cin>>data_input.salary;
 
+   return data_input;
 
 }
 
-void fill_vector(vector<stdata> &data){
+void fill_vector(vector<stdata> &vdata){
+	char choice = 'y';
 	int number=0 ;
 	do{
 		cout<<"How many employes: ";
 		number=enter_number();
 
 		for(int i=0; i<number; i++){
-			
+			vdata.push_back( fill_struct_data());
 		}
 
+           cout<<"\n do you want to add another employes? ";
+		   cin>>choice;
+	}while(choice=='y'||choice=='Y');
+}
+
+void vprint (vector<stdata> &vdata)
+{
+	cout<<"\t"<<"\n"<<"employes data"<<endl;
+	cout<<"\n_______________________________________________________________\n"<<endl;
+
+	for(stdata print :vdata ){
+
+		cout<<"first name: "<<print.first_name<<setw(2)<<"|"<<endl;
+		cout<<"last name: "<<print.last_name<<setw(2)<<"|"<<endl;
+		cout<<"age: "<<print.age<<setw(2)<<"|"<<endl;
+		cout<<"salary: "<<print.salary<<setw(2)<<"|"<<endl;
+		cout<<"\n________________________________________________\n\n";
 	}
+
+	cout<<"\n_______________________________________________________________\n"<<endl;
 }
 
 
-
 int main() {
-	
+
+	vector<stdata> vdata;
+	fill_vector(vdata);
+	vprint(vdata);
 }
