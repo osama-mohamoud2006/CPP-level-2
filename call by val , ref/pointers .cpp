9d6de7c5da75812946603723930ec22fd7b3c1df
osama mohamoud <wwwhdosama@gmail.com>
2025-08-04 21:02:40 +0300

    Auto commit: 2025-08-04 21:02:40

--- call by val , ref/pointers .cpp
+++ call by val , ref/pointers .cpp
@@ -4,11 +4,11 @@ using namespace std;
 int main(){
  int x = 20 ;
   int z =23; 
- int &y = x ; // takes the address of x 
+ int &y = x ; // takes the address,value of x 
 cout<<x<<" "<<y<<endl;
 
  int * X_p ;
- X_p = &x ; // pointer points at x (has the address of x )
+ X_p = &x ; // pointer points at x (store the address of x )
  X_p = &z ; 
  cout<<X_p<<endl; // will print address  
 
