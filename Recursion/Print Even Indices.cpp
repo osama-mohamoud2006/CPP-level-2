35c8cc4714661ab558367c7059528d2502738f12
osama mohamoud <wwwhdosama@gmail.com>
2025-08-04 14:54:11 +0300

    Auto commit: 2025-08-04 14:54:10

--- Recursion/Print Even Indices.cpp
+++ Recursion/Print Even Indices.cpp
@@ -2,34 +2,26 @@
 #include <vector>
 using namespace std;
 
-void vector_fill(vector<int>& numbers, int& n) {
-    int x;
-    cout << "Enter number of elements: ";
-    cin >> n;
-    if (n <= 0) return; 
-    for (int i = 0; i < n; ++i) {
-        cout << "Enter element #" << i + 1 << ": ";
-        cin >> x;
-        numbers.push_back(x);
+void output_even( vector<int> &numbers , int index){
+    //Print numbers in even indices
+ // index will start from vector size -1 
+    if(0>index) return ; // base case 
+    if(index%2==0) cout<<numbers[index]<<" ";
+
+    output_even(numbers , index -1);
+}
+
+int main(){
+    vector<int>numbers;
+    cout<<"enter the number of numbers you need: ";
+    int n =0 ;
+    cin>>n; // length 
+int number =0;
+    for(int i =0; i<n; i++){
+        cout<<"\nenter number ";
+        cin>>number;
+        numbers.push_back(number);
     }
-}
+     output_even( numbers ,  n-1);
 
-void find_even_order(const vector<int>& numbers, int index) {
-    if (index < 0) return;
-
-    if (index % 2 == 0)
-        cout << numbers[index] << " ";
-
-    find_even_order(numbers, index - 1);
-}
-
-int main() {
-    int n = 0;
-    vector<int> numbers;
-
-    vector_fill(numbers, n);
-
-    cout << "Output: ";
-    find_even_order(numbers, n - 1);
-    cout << endl;
 }
