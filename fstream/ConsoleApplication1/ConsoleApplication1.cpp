b29c5cfdfe65d30caa2aef8040e754e8b9d64ad4
osama mohamoud <wwwhdosama@gmail.com>
2025-08-09 20:57:58 +0300

    Auto commit: 2025-08-09 20:57:58

--- fstream/ConsoleApplication1/ConsoleApplication1.cpp
+++ fstream/ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,20 +5,20 @@ using namespace std;
 
 void print_file_to_screen(string file_pathfrom , string file_path_to="") {
 
-    // note file path string like "output_text.text"(as ex) the file path should be string so it is noraml what idid 
+    // note file path string like "output_text.text"(as ex) the file path should be string so it is noraml what i did 
     fstream file;
     fstream file2;
 
-    file.open(file_pathfrom, ios::in); // write mode
+    file.open(file_pathfrom, ios::in); // write mode (in --> means the data in file ) 
 
-    file2.open(file_path_to, ios::out | ios::app); // read mode 
+    file2.open(file_path_to, ios::out | ios::app); // read mode for file 2
 
     if (file.is_open()&& file2.is_open())  {
 
         string current_line = " ";
 
         while (getline(file, current_line)) {
-          
+           // make the output in the second file 
                 file2<< current_line << endl;
            
         }
--- fstream/readmode .cc
+++ fstream/readmode .cc
@@ -4,11 +4,11 @@
 using namespace std ;
 
 void print_file_to_screen(string file_path ){
-    
+
     // note file path string like "output_text.text"(as ex) the file path should be string so it is noraml what idid 
   fstream file ; 
   file.open(file_path , ios::in) ; // write mode
-
+// in means output in console 
 
   if(file.is_open()){
 
