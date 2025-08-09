ebbcc55598f1f38498f5877a8901f41b93b6916e
osama mohamoud <wwwhdosama@gmail.com>
2025-08-09 18:38:46 +0300

    Auto commit: 2025-08-09 18:38:46

---
+++ fstream/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <string> 
+using namespace std;
+
+void print_file_to_screen(string file_pathfrom , string file_path_to="") {
+
+    // note file path string like "output_text.text"(as ex) the file path should be string so it is noraml what idid 
+    fstream file;
+    fstream file2;
+
+    file.open(file_pathfrom, ios::in); // write mode
+
+    file2.open(file_path_to, ios::out | ios::app); // read mode 
+
+    if (file.is_open()&& file2.is_open())  {
+
+        string current_line = " ";
+
+        while (getline(file, current_line)) {
+          
+                file2<< current_line << endl;
+           
+        }
+    }
+
+}
+
+
+int main() {
+    print_file_to_screen("output_text2.text","output_text.text");
+}
