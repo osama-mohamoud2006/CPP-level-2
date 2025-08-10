3b45da1e2c8a3ee237f1d9135e739c601007cb43
osama mohamoud <wwwhdosama@gmail.com>
2025-08-10 15:46:24 +0300

    Auto commit: 2025-08-10 15:46:24

---
+++ fstream/file to vector.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+void file_to_vector(string path , vector<string> & vf)
+{
+    fstream file ;
+    file.open(path , ios::in); // read mode (ios::in reads file content)
+    if(file.is_open()){
+        string line = " "; 
+        while(getline(file , line)){
+            vf.push_back(line); // push each line to vector 
+           // cout<<line<<endl; // normal print file content on console screen 
+        }
+
+    file.close();
+    }
+}
+
+int main(){
+    vector<string>  vf;
+file_to_vector("id.text" ,  vf);
+    for(string &line: vf){
+        cout<<line<<endl;
+    }
+}
