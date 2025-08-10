19d3008d84cba0cb1598c1b5ee7aa95aa92ffff7
osama mohamoud <wwwhdosama@gmail.com>
2025-08-10 23:22:38 +0300

    Auto commit: 2025-08-10 23:22:37

---
+++ time/print full time details into file.cpp
@@ -0,0 +1,67 @@
+ /* 
+  int tm_sec; // seconds of minutes from 0 to 61
+  int tm_min; // minutes of hour from 0 to 59
+  int tm_hour; // hours of day from 0 to 24
+  int tm_mday; // day of month from 1 to 31
+  int tm_mon; // month of year from 0 to 11
+  int tm_year; // year since 1900
+  int tm_wday; // days since sunday
+  int tm_yday; // days since January 1st
+  int tm_isdst; // hours of daylight savings time
+*/  
+
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <fstream>
+#include <string>
+using namespace std;
+
+struct sttime{
+    int day=0 ; 
+    int month =0;
+    int year =0; 
+};
+
+sttime hardcoded_struct(){
+      time_t detailed_time = time(0);
+    tm* print_detailed_time = localtime(&detailed_time);
+    sttime time ; 
+    time.day =print_detailed_time->tm_mday;
+    time.month =print_detailed_time->tm_mon+1;
+    time.year=print_detailed_time->tm_year+1900;
+   
+
+}
+
+void fill_vector_with_strucr(){
+    
+}
+void write_to_File(string path ){
+    fstream timefile;
+    timefile.open(path , ios::out); // writing mode
+    if(timefile.is_open()){
+       
+      time_t detailed_time = time(0);
+    tm* print_detailed_time = localtime(&detailed_time);
+    timefile<<"the day in month is: "<<print_detailed_time->tm_mday<<endl;
+    timefile<<"the month is: "<<print_detailed_time->tm_mon+1<<endl;
+    timefile<<"the year is: "<<print_detailed_time->tm_year+1900<<endl;
+    timefile<<"\n\n"<<print_detailed_time->tm_mday<<"/"<<print_detailed_time->tm_mon<<"/"<<print_detailed_time->tm_yday<<endl;
+
+    timefile<<"\n\n"<<"the hour is: "<<print_detailed_time->tm_hour<<endl;
+    timefile<<"the min is: "<<print_detailed_time->tm_min<<endl;
+    timefile<<"the sec is: "<<print_detailed_time->tm_sec<<endl;
+
+ timefile<<"\n\n"<<print_detailed_time->tm_hour<<":"<<print_detailed_time->tm_min<<":"<<print_detailed_time->tm_sec<<endl;
+
+ timefile.close();
+
+
+        
+    }
+}
+
+int main(){
+ write_to_File( "time.text" );
+}
