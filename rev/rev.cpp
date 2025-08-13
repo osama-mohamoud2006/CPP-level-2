2b1a9ac1bf1f9c59af3476876f402da5fd8c85dd
osama mohamoud <wwwhdosama@gmail.com>
2025-08-13 21:09:55 +0300

    Auto commit: 2025-08-13 21:09:55

--- rev/rev.cpp
+++ rev/rev.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
-using namespace std;
-
+void c() {
+    int c = 10;
+    int m = 20; 
+}
 int main() {
-    int a = 10;
-    int b = 0;
-    int c = a / 10; // ?? Runtime error: Division by zero
-    cout << c;
+    int a = 5;  // step into --> line by line 
+    int b = 3;
+    c(); // step over -- >execute the function without going inside it line by line 
+    int sum = a + b;
     return 0;
 }
