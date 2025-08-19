00da6e1d3a71a7c7d5d73a048a5fbd2ad00f2fc7
osama mohamoud <wwwhdosama@gmail.com>
2025-08-19 13:46:32 +0300

    Auto commit: 2025-08-19 13:46:31

--- rev/create ref.cc
+++ rev/create ref.cc
@@ -3,6 +3,9 @@ using namespace std;
 void x2(int y){
     y++;
 }
+void x3(int &y){
+    y++;
+}
 int main(){
     // create ref 
     int a=10; 
@@ -12,4 +15,12 @@ int main(){
     x2(a);//will copy the value of a ,
     // and will create new var -y- (diff address , 
     //the same value , local change )
+    cout<<a<<endl;
+
+    x3(x); // the a , y will be the same address , the same value , so 
+    // any changes happen to a will affect y and any affect on y 
+    // will affect on a (&y=a )
+     cout<<a<<endl;
+     a=100; 
+      cout<<x<<endl;
 }
