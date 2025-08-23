0a4b11a9c69b1b492bebb1356ed310f0063d3110
osama mohamoud <wwwhdosama@gmail.com>
2025-08-23 14:52:58 +0300

    Auto commit: 2025-08-23 14:52:58

--- rev/time now(local time).cc
+++ rev/time now(local time).cc
@@ -5,14 +5,7 @@ using namespace std;
 int main()
 {
 
-    local time
-    time_t timeNOW_in_sec_since_1970 = time(0);
-    char *timeINString = ctime(&timeNOW_in_sec_since_1970);
-    cout << timeINString << endl;
-       /UTC time
-    tm *gmt_time = gmtime(&timeNOW_in_sec_since_1970);
-    char *print_gmt = asctime(gmt_time);
-    cout << print_gmt << endl;
+   
 
     time_t time_epoch= time(0);
     tm * UTC = gmtime(&time_epoch); // convert epoch to struct tm
